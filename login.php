0e36a75053632809bc556c7dcb8194288fd6b79b
Merilin Takk <meritak@greeny.cs.tlu.ee>
2015-09-09 21:24:11 +0300

    loo kasutaja

--- login.php
+++ login.php
@@ -24,25 +24,27 @@
 			if(strlen($_POST["password"]) < 8){
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
 			
+			   
 				
 			}
 		}
 		
 		
 		//kontrollin, et parool ei ole tühi
+		
 	}
 	
-	
     
-		
 ?>
+
+
 <html>
 <head>
     <title>Login page</title>
 </head>
 <body>
     
-	<h2>Log in</h2>
+	<h2>Logi sisse</h2>
 	
 	<form action="login.php" method="post" >
 	<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
@@ -50,7 +52,28 @@
 	<input type ="submit" value="log in">
 	</form>
 	
-<h2>Create user</h2>
+<h2>Loo kasutaja</h2>
+
+<form id='register' action='register.php' method='post'
+    accept-charset='UTF-8'>
+<fieldset >
+<legend>Register</legend>
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+<label for='name' >Your Full Name*: </label>
+<input type='text' name='name' id='name' maxlength="50" />
+<label for='email' >Email Address*:</label>
+<input type='text' name='email' id='email' maxlength="50" />
+ 
+<label for='username' >UserName*:</label>
+<input type='text' name='username' id='username' maxlength="50" />
+ 
+<label for='password' >Password*:</label>
+<input type='password' name='password' id='password' maxlength="50" />
+<input type='submit' name='Submit' value='Submit' />
+ 
+</fieldset>
+</form>
+
 	
 </body
 </html>
