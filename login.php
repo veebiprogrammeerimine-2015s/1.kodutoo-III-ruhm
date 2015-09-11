8d75cce7bfe9b6f0608febd5fe72c7282fffd032
raitavastu <ravastu@tlu.ee>
2015-09-11 12:35:05 +0300

    added registration form

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $error = "";
-        $email_error = "";
-        $pswd_error = "";
-
-        if(empty($_POST['username'])){
+$email_error = "";
+$pswd_error = "";
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if($_POST['submit'] == 'login'){
+        if(empty($_POST['email'])){
             $email_error = "Email kohustuslik!";
         }
 
@@ -12,21 +11,35 @@
             $pswd_error = "Password kohustuslik!";
         }
 
-        $username = $_POST['username'];
+        $email = $_POST['email'];
         $password = $_POST['password'];
         echo $username;
         echo $password;
-
     }
+}
 ?>
-
+<!DOCTYPE html>
 <html>
-    <head></head>
-    <body>
-        <form action="login.php" method="post">
-            <input type="email" name="username" id="username" placeholder="Email"><? echo $email_error;?>
-            <input type="password" name="password" id="password" placeholder="password"><? echo $pswd_error;?>
-            <input type="submit" name="login" id="login" value="Login">
-        </form>
-    </body>
+    <head>Kodune töö 1</head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div id="login_form">
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <input type="email" name="username" id="email" placeholder="Email"><? echo $email_error;?><br>
+        <input type="password" name="password" id="password" placeholder="password"><? echo $pswd_error;?><br>
+        <input type="submit" name="login" class="login" value="Login">
+    </form>
+</div>
+
+<div id="registration_form">
+    <h1>Register new user</h1>
+    <form action="login.php" method="post">
+        <input type="email" name="username" placeholder="Email"><? echo $email_error;?><br>
+        <input type="password" name="password" placeholder="password"><? echo $pswd_error;?><br>
+        <input type="text" name="eesnimi" placeholder="password"><? echo $pswd_error;?><br>
+        <input type="submit" name="resgister" class="login" value="Login">
+    </form>
+</div>
+</body>
 </html>
