70ca2211b9990830621d68d3426ce2d600365465
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-09-15 19:38:22 +0300

    lisasin asjad

--- login.php
+++ login.php
@@ -14,7 +14,9 @@
 		if ( empty($_POST["email"]) ) {
 			$email_error = "See väli on kohustuslik";
 		}
-		
+		if ( empty($_POST["date"]) ) {
+			$date_error = "See väli on kohustuslik";
+		}
 		//kontrollin, et parool ei ole tühi
 		if ( empty($_POST["password"]) ) {
 			$password_error = "See väli on kohustuslik";
@@ -41,12 +43,14 @@
 		</form>
 	
 	<h2>Create user</h2>
+	Tärniga märgitud lahtrid on kohustuslikud
 	<form action="login.php" method="post" >
-	<input name="email" type="email" placeholder="Email ">
-	<input name="pass" type="password" placeholder="Parool ">
-	<input name="name" type="first_name" placeholder="Eesnimi">
-	<input name="name" type="last_name" placeholder="Perekonnanimi">
-	<input name="date" type="date" placeholder="Sünniaeg ">
+	<input name="email" type="email" placeholder="Email ">*<?php echo $email_error ?><br><br>
+	<input name="pass" type="password" placeholder="Parool ">*<?php echo $password_error ?><br><br>
+	<input name="name" type="first_name" placeholder="Eesnimi"><br><br>
+	<input name="name" type="last_name" placeholder="Perekonnanimi"><br><br>
+	<input name="date" type="date" placeholder="Sünniaeg ">*<?php echo $date_error ?><br>
+	<input type="submit" value="Edasta">
 	</form>
 </body>
 </html>
