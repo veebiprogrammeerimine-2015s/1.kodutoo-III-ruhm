2c56418670e99bbd41965339bb540c4e514d50b3
Aivar Russki <aivarus@greeny.cs.tlu.ee>
2015-09-15 20:52:54 +0300

    Lisasin veateateid

--- login.php
+++ login.php
@@ -15,7 +15,18 @@
 		if ( empty($_POST["email"]) ) {
 			$email_error = "See väli on kohustuslik";
 		}
-		
+		//Kontrollin, et nimi ei ole tühi
+			if ( empty($_POST["eesnimi"]) ) {
+				$eesnimi_error = "See väli on kohustuslik";
+			}
+			
+			if ( empty($_POST["omanimi"]) ) {
+				$omanimi_error = "See väli on kohustuslik";
+			}
+			
+			if ( empty($_POST["kuupäev"]) ) {
+				$kuupäev_error = "See väli on kohustuslik";
+			}
 		//kontrollin, et parool ei ole tühi
 		if ( empty($_POST["password"]) ) {
 			$password_error = "See väli on kohustuslik";
@@ -26,11 +37,6 @@
 			if(strlen($_POST["password"]) < 8) { 
 			
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
-			
-			//Kontrollin, et perekonnanimi ei ole tühi
-			if ( empty($_POST["Omanimi"]) ) {
-				$omanimi_error = "See väli on kohustuslik";
-			}
 			
 			}
 	
@@ -56,8 +62,8 @@
 	<h2>Create user</h2>
 	
 		<form action="signup.php" method="post" >
-			<input name="oma nimi" type="omanimi" placeholder="Eesnimi"> <?php echo $eesnimi_error; ?><br><br>
-			<input name="oma nimi" type="omanimi" placeholder="perekonnanimi"> <?php echo $omanimi_error; ?> <br><br>
+			<input name="eesnimi" type="eesnimi" placeholder="Eesnimi"> <?php echo $eesnimi_error; ?><br><br>
+			<input name="omanimi" type="omanimi" placeholder="perekonnanimi"> <?php echo $omanimi_error; ?> <br><br>
 			<input name="kuupäev" type="kuupäev" placeholder="kuupäev"> <?php echo $kuupäev_error; ?><br><br>
 			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
 			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
