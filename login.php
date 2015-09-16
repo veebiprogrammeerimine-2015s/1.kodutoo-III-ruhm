9384052049d07e7b7add67501d48c1ec8e813dc5
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-16 09:32:03 +0300

    MVP katse

--- login.php
+++ login.php
@@ -1,10 +1,8 @@
 <?php
 
 	// LOGIN.PHP
-	$email_error = "";
-	$passw_error = "";
-	$fname_error = "";
-	$lname_error = "";
+	$email_error = $passw_error = $fname_error = $lname_error = $email2_error = $passw2_error ="";
+	$email = $passw = $fname = $lname ="";
 	//echo $_POST["email"];
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"]=="POST"){
@@ -31,13 +29,13 @@
 			$lname_error = "see väli on kohustulik";
 		}	
 		if (empty($_POST["email"])){
-			$email_error = "see väli on kohustulik";	
+			$email2_error = "see väli on kohustulik";	
 		}
 		if (empty($_POST["password"])){
-			$passw_error = "see väli on kohustulik";	
+			$passw2_error = "see väli on kohustulik";	
 		} else {			
 			if(strlen($_POST["password"]) < 8){
-				$passw_error="peab olema vähemalt 8 tähemärki";
+				$passw2_error="peab olema vähemalt 8 tähemärki";
 			}
 		}	
 	}
@@ -59,8 +57,8 @@
 	<h2>Create user</h2>
 	
 		<form action="login.php" method="post">
-			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
-			<input name="password" type="password" placeholder="Parool"> <?php echo $passw_error; ?> <br><br>
+			<input name="email2" type="email" placeholder="E-post" <?php echo $email2_error; ?> ><br><br>
+			<input name="password2" type="password" placeholder="Parool"> <?php echo $passw2_error; ?> <br><br>
 			<input name="first name" type="text" placeholder="Eesnimi"> <?php echo $fname_error; ?><br><br>
 			<input name="last name" type="text" placeholder="Perekonnanimi"> <?php echo $lname_error; ?><br><br>
 			Sugu:
@@ -69,7 +67,7 @@
 			<input name="vanus" type="number_format" placeholder="Vanus"><br><br>
 			<input type="submit" value="Submit"> <br><br>
 		</form>	
-	
+	<h2>Minu mvp idee. Lehekülg kus saab </h2>
 </body>
 
 
