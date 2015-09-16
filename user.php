c79a16ba2990f1d9dcc25834c75616221804181e
Karin Salu <kar1ns@greeny.cs.tlu.ee>
2015-09-16 09:12:15 +0300

    parandus

--- user.php
+++ user.php
@@ -1,7 +1,8 @@
 <?php
 	//login.php
-	$name_error = "";
+	$email_error = "";
 	$password_error ="";
+	$name_error ="";
 	
 	//echo $_POST["email"];  //ANNAB MUUTUJA MIS AADRESSIREAL ON
 	// kontrollime et keeegi vajutas input nuppu
@@ -11,7 +12,7 @@
 		//kontrollin et e-mail ei ole tühi
 		
 		if( empty($_POST["email"])){
-			$name_error = "See väli on kohustuslik";
+			$email_error = "See väli on kohustuslik";
 		}
 		//kontrollin, et parool ei ole tühi
 		
@@ -22,6 +23,9 @@
 			if(strlen ($_POST["password"]) < 8){
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
 			}
+			if( empty($_POST["name"])){
+			$name_error = "See väli on kohustuslik";
+		}
 		}
 	}
 	
@@ -30,14 +34,16 @@
 ?>
 <html>
 <head>
-	<title>Signeup page</title>
+	<title>Signup page</title>
 </head>
 <body>
 	<h2>Creat account</h2>
 	
-		<form action="login.php" method="post">
+		<form action="user.php" method="post">
+			<input name="First" type="email" placeholder="Email"> <?php echo $email_error; ?><br><br>
 			<input name="name" type="name" placeholder="Name"> <?php echo $name_error; ?><br><br>
 			<input name="password"type="password" placeholder="Password"> <?php echo $password_error; ?> <br><br>
+			<input name="email" type="email" placeholder="Email"> <?php echo $email_error; ?><br><br>
 			<input type="submit" value="Log in">
 		</form>
 	<h2>Creat user</h2>
