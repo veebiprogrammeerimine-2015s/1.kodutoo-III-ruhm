35a6b606d1aecb44452486690764672918c2a39d
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-09-16 13:38:22 +0300

    Final fixes and idea

--- login.php
+++ login.php
@@ -6,6 +6,9 @@
 		// errori muutujad peavad igal juhul olemas olema
 		$email_error = "";
 		$password_error= "";	
+		$username_error= "";
+		$email1_error= "";
+		$password1_error="";
 		
 		//kontrollime et keegi vajutas input nuppu
 		if($_SERVER["REQUEST_METHOD"] == "POST")  {
@@ -29,6 +32,25 @@
 				}
 				
 			}
+			
+			if (empty($_POST["username"]) ) {
+				$username_error = "Kirjuta oma kasutajanimi";
+			}
+			if (empty($_POST["email1"]) ) {
+				$email1_error = "Kirjuta oma email";
+			}
+			if (empty($_POST["password1"]) ) {
+				$password1_error= "Kirjuta parool";
+			} else {
+			
+				if(strlen($_POST["password1"]) < 8) {
+					$password1_error= "Peab olema vähemalt 8 tähemärki pikk"; 
+				}
+				
+			}
+			
+			
+			
 		}
 		
 
@@ -38,7 +60,7 @@
 	<title>Login page</title>
 </head>
 <body>
-<
+
 	<h2>Login</h2>
 		<form action="login.php" method="post" >
 			<input name="email" type="email" placeholder="E-post"><?php echo $email_error; ?><br><br>
@@ -48,11 +70,13 @@
 		
 	<h2>Loo kasutaja</h2>
 		<form action="login.php" method="post">
-			<input name="kasutaja nimi" type="text" placeholder="Kasutaja nimi"> või <input name="email" type="email" placeholder="E-post"><br></br>
-			<input name="password1" type="password" placeholder="Parool"><?php echo $password_error;?> <br></br>
+			<input name="create_username" type="text" placeholder="Kasutaja"><?php echo $username_error; ?><br></br>  
+			<input name="create_email" type="email" placeholder="E-post"><?php echo $email_error;?> <br></br>
+			<input name="create_password" type="password" placeholder="Parool"><?php echo $password_error;?> <br></br>
 			<input type="submit" value="Loo kasutaja">
 		</form>
+
+	Minu mvp ideeks oleks midagi redditi sarnast või siis mingi foorumi taoline leht. Foorumis saaks igaüks oma teema luua ja vastavalt selle oma pilte või mõtteid postitada. Kui sellest asja ei saa siis võiks mingi teemale see foorumi
+		keskenduda(näiteks arvuti,internet jne.).
 </body>
-
-
 </html>
