2c4bbd731d04ee06eb052af47429de5f9ac495d3
Merilin Takk <meritak@greeny.cs.tlu.ee>
2015-09-16 12:11:20 +0300

    fix

--- login.php
+++ login.php
@@ -4,16 +4,23 @@
 	
 	$email_error = "";
 	$password_error = "";
+	$name_error = "";
+	$username_error = "";
 	
 	//kontrollime, et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"]== "POST"){
 		//echo "keegi vajutas nuppu";
 		
 		//kontrollin, et e-post ei ole tühi
-	if(empty($_POST["email"]) ){
+		if(empty($_POST["email"]) ){
 		$email_error = "See väli on kohustuslik";
 		}
-		
+		if(empty($_POST["usename"]) ){
+		$username_error = "See väli on kohustuslik";
+		}
+		if(empty($_POST["name"]) ){
+		$name_error = "See väli on kohustuslik";
+		}
 		//kontrollin, et parool ei ole tühi
 		if(empty($_POST["password"]) ){
 		$password_error = "See väli on kohustuslik";	
@@ -24,46 +31,39 @@
 			if(strlen($_POST["password"]) < 8){
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
 			
-			   
-				
-			}
-		}
+			
+			}  
+			
 		
-		
+		}	
 		//kontrollin, et parool ei ole tühi
-		
-	}
+	}	
 	
-    
 ?>
-
-
-<html>
-<head>
-    <title>Login page</title>
-</head>
-<body>
+<?php require_once("../header.php"); ?>
     <h2>Registreeri!</h2>
 
-<form id='register' action='register.php' method='post'
+<form id='register' action='login.php' method='post'
     accept-charset='UTF-8'>
 <fieldset >
 <legend>Registreeri!</legend>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 <label for='name' >Nimi*: </label>
-<input type='text' name='name' id='name' maxlength="50" />
+<input type='text' name='name' id='name' maxlength="50" /><?php echo $name_error; ?> <br><br>
 <label for='email' >E-posti aadress*:</label>
-<input type='text' name='email' id='email' maxlength="50" />
+<input type='text' name='email' id='email' maxlength="50" /><?php echo $email_error; ?> <br><br>
  
 <label for='username' >Kasutajanimi*:</label>
-<input type='text' name='username' id='username' maxlength="50" />
+<input type='text' name='username' id='username' maxlength="50" /><?php echo $username_error; ?> <br><br>
  
 <label for='password' >Parool*:</label>
-<input type='password' name='password' id='password' maxlength="50" />
+<input type='password' name='password' id='password' maxlength="50" /><?php echo $password_error; ?> <br><br>
 <input type='submit' name='Registreeru!' value='Registreeru!' />
  
 </fieldset>
 </form>
+
+
 
 	<h2>Logi sisse</h2>
 	
@@ -74,21 +74,7 @@
 	</form>
 	
 	
-</body
-</html>
-
-<html>
-<head>
-<style>
-body {background-color:lightblue}
-h1   {color:black}
-p    {color:black}
-</style>
-</head>
-<body>
-
 <h1>Pealkiri - teemale</h1>
 <p>alltekst,mis toimuma hakkab.</p>
 
-</body>
-</html>
+<?php require_once("../footer.php"); ?>
