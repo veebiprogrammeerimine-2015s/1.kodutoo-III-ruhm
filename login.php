7e2a810964d20badf22ec45e4a065aedba2be65f
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2015-09-20 22:42:15 +0300

    modified definitions

--- login.php
+++ login.php
@@ -5,9 +5,13 @@
 	$email_error = "";
 	$password_error = "";
 	$name_error = "";
+	$password_error2 = "";
+	$email_error2 = "";
+	$first_name_error = "";
+	$last_name_error = "";
+	$password_error3 = "";
 	
-	
-	
+	echo date ("l, j. F, Y, e");
 	
 	//kontrollime, et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST" ) {
@@ -37,22 +41,28 @@
 	if($_SERVER["REQUEST_METHOD"] == "POST" ) {
 		
 		if (empty($_POST["e-mail"])){
-			$email_error = "See väli on kohustuslik";
+			$email_error2 = "See väli on kohustuslik";
 		}
 		if (empty($_POST["pass"]) ) {
-			$password_error = "See väli on kohustuslik";
+			$password_error2 = "See väli on kohustuslik";
 		} else {
 			
 			
 
 			if(strlen($_POST["pass"]) <8) {
-				$password_error = "Parool on liiga lühike, peab olema vähemalt 8 tähemärki pikk!";
+				$password_error3 = "Parool on liiga lühike, peab olema vähemalt 8 tähemärki pikk!";
 			}
 	
 		}
-		if (empty ($_POST["name"])){
-			$name_error = "See väli on kohustuslik";
+		if (empty ($_POST["first_name"])){
+			$first_name_error = "See väli on kohustuslik";
 		}
+		}if (empty ($_POST["last_name"])){
+			$last_name_error = "See väli on kohustuslik";
+	
+	
+	
+	
 	}	
 ?>
 
@@ -77,11 +87,11 @@
 		
 	<h2>Create user</h2>
 		
-		<form action="create_user.php" method="post" >
-			<input name="e-mail" type="email" placeholder="e-mail" > <?php echo $email_error; ?> <br><br>
-			<input name="pass" type="password" placeholder="password" > <?php echo $password_error; ?> <br><br><br><br>
-			<input name="name" type="name" placeholder="first name" > <?php echo $name_error; ?> <br><br>
-			<input name="name" type="name" placeholder="last name" > <?php echo $name_error; ?> <br><br>
+		<form action="login.php" method="post" >
+			<input name="e-mail" type="email" placeholder="e-mail" > <?php echo $email_error2; ?> <br><br>
+			<input name="pass" type="password" placeholder="password" > <?php echo $password_error2; ?> <br><br><br><br>
+			<input name="first_name" type="name" placeholder="first name" > <?php echo $first_name_error; ?> <br><br>
+			<input name="last_name" type="name" placeholder="last name" > <?php echo $last_name_error; ?> <br><br>
 			<input type="submit" value="Create user" >
 		</form>	
 			
