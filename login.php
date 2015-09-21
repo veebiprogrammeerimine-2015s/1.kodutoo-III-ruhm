f46d04b0e830aa77080b624cab50f50558412be8
Henrik Romanenkov <henrrom@greeny.cs.tlu.ee>
2015-09-21 11:24:35 +0300

    Parandamine

--- login.php
+++ login.php
@@ -11,19 +11,20 @@
 		
 		//kontrollin, et e-post ei ole tühi
 		if(empty($_POST[$email]) ){
-			$email_error = " See väli on kohustuslik."
+			$email_error = " See väli on kohustuslik.";
 		}
 		
 		//kontrollin, et parool ei ole tühi
 		if(empty($_POST[$password]) ){
-			$password_error = "See väli on kohustuslik."
-		}else {	
+			$password_error = "See väli on kohustuslik.";
+		}else{
 			
 			// kui oleme siia jõudnud, siis parool ei ole tühi
 			//kontrollin, et oleks vähemalt 8 sümbolit pikk
-			strlen
+			
 			if(strlen($_POST[$password]) < 8) {
-				$password_error = "Peab olema vähemalt 8 tähemärki pikk"
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk";
+			}
 		}
 	}
 ?>
@@ -34,14 +35,14 @@
 </head>
 <body>
 	<h2>Log in</h2>
-		<form action="Login.php" method="post">
+		<form action="login.php" method="post">
 			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
 			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 			<input type="submit" value="Log in"> 
 		</form>
 		
 		
-	<h2>Create useer</h2>
+	<h2>Create user</h2>
 </body>
 
 </html>
