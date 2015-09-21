44f95aac52dfa266aac5436f776ac60231df028a
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2015-09-21 19:09:46 +0300

    final katse 24

--- login.php
+++ login.php
@@ -31,19 +31,7 @@
 				}
 				
 			}
-			if (empty($_POST["password2"])) {
-				
-				$pw_error = "See väli on kohustuslik";
-				
-				
-			} else {
-				
-				//parool ei ole tühi ja kontrollime mitu tähemärki on
-				if(strlen($_POST["password2"]) < 8 ) {
-					$pw_error = "Parool peab olema vähemalt 8 tähemärki pikk!";
-				}
-				
-			}
+		
 			
 		if (empty($_POST["username"])) {
 				
@@ -81,7 +69,7 @@
 		
 	<form action="login.php" method="post" >
 		<input type="text" name="username" placeholder="Kasutajanimi"> <?php  echo $email_error;  ?> <br><br>
-		<input type="password" name="password2" placeholder="Parool"> <?php  echo $pw_error;  ?> <br><br>
+		<input type="password" name="password" placeholder="Parool"> <?php  echo $pw_error;  ?> <br><br>
 		<input type="email" name="email2" placeholder="E-mail"> <?php echo $email_error; ?> <br> <br>
 		<input type="submit" value="Registreeri"> <br><br>
 	</form>
