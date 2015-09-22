6a63f2d89e37cae42837ce6d5c66959936b3be3f
Henrik Romanenkov <henrrom@greeny.cs.tlu.ee>
2015-09-22 21:43:31 +0300

    new file

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php
  
 	//echo$_POST["email"];
@@ -5,55 +6,113 @@
 	
 	$email_error = "";
 	$password_error = "";
+	
 	$name_error = "";
 	
 	//muutujad ab väärtuste jaoks
 	$email = "";
 	$name = "";
+	
 	//kontrollime, et keegi vajutas input nuppu.
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
 		//echo "Keegi vajutas nuppu";
 		
-		//kontrollin, et e-post ei ole tühi
-		if(empty($_POST[$email]) ){
-			$email_error = " See väli on kohustuslik.";
-		}
 		
-		//kontrollin, et parool ei ole tühi
-		if(empty($_POST[$password])){
-			$password_error = "See väli on kohustuslik.";
-		}else{
+		//keegi vajutas login nuppu
+		if(isset($_POST["login"])){
 			
-			// kui oleme siia jõudnud, siis parool ei ole tühi
-			//kontrollin, et oleks vähemalt 8 sümbolit pikk
+			echo "Vajutas login nuppu!";
 			
-			if(strlen($_POST[$password]) < 8) {
-				$password_error = "Peab olema vähemalt 8 tähemärki pikk";
+			//kontrollin, et e-post ei ole tühi
+			if(empty($_POST[$email]) ){
+				$email_error = " See väli on kohustuslik.";
+			}else{
+				
+				$email = test_input($_POST["email"]);
+			
+			}	
+				
+			//kontrollin, et parool ei ole tühi
+			if(empty($_POST[$password])){
+				$password_error = "See väli on kohustuslik.";
+			}else{
+				
+				// kui oleme siia jõudnud, siis parool ei ole tühi
+				// kontrollin, et oleks vähemalt 8 sümbolit pikk
+				if(strlen($_POST[$password]) < 8) {
+					
+					$password_error = "Peab olema vähemalt 8 tähemärki pikk";
+				}
+			
+			
 			}
+			
+			//kontrollin et ei oleks ühtegi errorit
+			if($email_error == ""&& $password_eror ==""){
+				
+				echo "kontrollin sisselogimist".$email." ja parool ";
+			}
+			
+			
+			
+			
+		// keegi vajutas create  nuppu
+		}elseif(isset($_POST["create"])){
+			
+			echo "Vajutas create nuppu!";
+				
+			//valideerimine create user vormile
+			//kontrollin, et nimi ei ole tühi
+			if ( empty($_POST["name"]) ) {
+				$name_error = "See väli on kohustuslik";
+			}else{
+				//kõik korras
+				//test_input eemaldab pahatahtlikud osad
+				$name = test_input($_POST["name"]);
+					
+			}
+			if($name_error == ""){
+				echo "salvestan ab'i".@name;
+			}
+ 		
+		
 		}
-	}
-	//mvp-tahaksin teha midagi Instagram´i laadset. Lehe, kus saaks pilte üles laadida.
-?>
-<html>
-<head>
-	<title>Login page</title>
+	}	
+	function test_input($data) {
+		//võtab ära tühikud, enterid ja tabid
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+		
+
+
+
+
+
 	
-</head>
-<body>
-	<h2>Log in</h2>
+	}
+?>
+<?php
+	$page_title = "Sisselogimise leht";
+	$page_file_name = "login.php";
+?>
+<?php require_once("../header.php"); ?>
+	<h2>Sisselogimine</h2>
+	
 		<form action="login.php" method="post">
-			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+			<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?> <br><br>
 			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
-			<input type="submit" value="Log in"> 
+			<input name="login" type="submit" value="Log in"> 
 		</form>
 		
 	<h2>Kasutaja loomine</h2>
 		<form action="login.php" method="post">
 			<input name="name" type="text" placeholder="Eesnimi Perekonnanimi" value="<?php echo $name; ?>">* <?php echo$name_error; ?><br><br>
 			<input name="login" type="sumbit" value="Create">
-	
 		</form>
 		
-</body>
-
-</html>
+		
+		
+<?php require_once("../footer.php"); ?>
