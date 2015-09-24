ea646c033a6ff25199d44547d92298ec2ac69cca
Kadri Alumets <kadralu@greeny.cs.tlu.ee>
2015-09-25 00:26:40 +0300

    update

--- login.php
+++ login.php
@@ -20,17 +20,17 @@
 	if($_SERVER["REQUEST_METHOD"]== "POST") {
 		//echo "Keegi vajutas nupppu";
 		
-		if(empty($_POST["email1"])) {
-			$email1_error = "See väli on kohustuslik";
+		if(empty($_POST["email"])) {
+			$email_error = "See väli on kohustuslik";
 		}
 		//Kontrollin, et parool ei ole tühi
-		if(empty($_POST["password1"])) {
-			$password1_error = "See väli on kohustuslik";
+		if(empty($_POST["password"])) {
+			$password_error = "See väli on kohustuslik";
 		}	else {
 			//kui siia jõudnud siis pole tühi
 			//Kontrollin parooli pikkust
-			if(strlen($_POST["password1"]) <8) {
-				$password1_error = "Peab olema vähemalt 8 tähemärki pikk!";
+			if(strlen($_POST["password"]) <8) {
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
 			}
 		}
 	}
@@ -90,8 +90,8 @@
 <body>
 	<h2>Log in</h2>
 	<form action="login.php" method="post" >
-		<input name="email1" type="email" placeholder="E-post"> <?php echo $email1_error; ?> <br><br>
-		<input name="password1" type="password" placeholder="Parool"> <?php echo $password1_error; ?> <br><br>
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 		<input type ="submit" value="Logi sisse">
 	</form>
 	<h2>Create user</h2>
