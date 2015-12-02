bb37d979904ff1d02a017eca57756c3877987c17
Helen Puhu <helepuh@greeny.cs.tlu.ee>
2015-12-02 11:54:33 +0200

    lisasin idee

--- login.php
+++ login.php
@@ -13,10 +13,18 @@
 			$email_error = "See väli on kohustuslik";
 		}
 		//kontrolln, et parool ei oleks tühi
+		if ( empty($_POST["firstname"]) ) {
+			$firstname_error = "See väli on kohustuslik";
+		}
+		if ( empty($_POST["lastname"]) ) {
+			$lastname_error = "See väli on kohustuslik";
+		}
 		
 		if ( empty($_POST["password"]) ) {
 			$password_error = "Parool on kohustuslik";
 		} else {
+		
+		
 		
 			if(strlen($_POST["password"]) < 8) {
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
@@ -32,12 +40,26 @@
 <body>
 	<h2>Log in</h2>
 		<form action="login.php" method="post">
-		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error ?> <br> <br>
+		<input name="email" type="email" placeholder="E-post">  <?php echo $email_error ?> <br> <br>
 		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error ?> <br> <br>
 		<input type="submit" value="Log in"> <br> <br>
 		</form>
-	<h2>Create user </h2>
-	
+		<h2>Create user </h2>
+
+
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error ?> <br> <br>
+
+		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error ?> <br> <br>
+
+		<input name="firstname" type="firstname" placeholder="Eesnimi"> <?php echo $firstname_error ?> <br> <br>
+		<input name="lastname" type="lastname" placeholder="Perekonnanimi"> <?php echo $lastname_error ?> <br> <br>
+		<input type="submit" value="Sign up"> <br> <br>
+<p>Minu ideeks oli loomade varjupaiga leht, kus saab hoida järge, kes on varjupaigas, kes juba kodu leidnud jne </p>
+		</form>
 	
 </body>
 </html>
+
+
+
+
